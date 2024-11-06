b31e32849bd41b185a60220a24d1043037ff7b35
BigJMC9 <Jake@crosbie.net.au>
2024-11-06 20:31:38 +1100

    Updated Submodules

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,8 @@
 project "yaml-cpp"
-<<<<<<< HEAD
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"	
-	staticruntime "on"
-=======
-	kind "SharedLib"
-	language "C++"
-	cppdialect "C++17"	
 	staticruntime "off"
->>>>>>> d147c3bc08f3c3828919bd6d6e714cd27e43bfde
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -18,7 +11,6 @@ project "yaml-cpp"
 	{
 		"src/**.h",
 		"src/**.cpp",
-		
 		"include/**.h"
 	}
 
@@ -27,38 +19,17 @@ project "yaml-cpp"
 		"include/"
 	}
 
-<<<<<<< HEAD
-	defines
-	{
-		"YAML_CPP_STATIC_DEFINE"
-	}
-
-=======
->>>>>>> d147c3bc08f3c3828919bd6d6e714cd27e43bfde
 	filter "system:windows"
 		systemversion "latest"	
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
-<<<<<<< HEAD
-=======
-		defines "yaml_cpp_EXPORTS"
->>>>>>> d147c3bc08f3c3828919bd6d6e714cd27e43bfde
 
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-<<<<<<< HEAD
-=======
-		defines "yaml_cpp_EXPORTS"
->>>>>>> d147c3bc08f3c3828919bd6d6e714cd27e43bfde
 
 	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
-<<<<<<< HEAD
-=======
-		symbols "off"
-		defines "yaml_cpp_EXPORTS"
->>>>>>> d147c3bc08f3c3828919bd6d6e714cd27e43bfde
